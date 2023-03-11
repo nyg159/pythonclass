8e36d943810affaef9a64c4cb4adecd57f703182
nyg159 <nyg159@gmail.com>
2023-03-11 11:40:32 +0900

    수정

--- 1.1.7.py
+++
@@ -1,9 +0,0 @@
-import turtle
-t = turtle.Turtle()
-t.shape("turtle")
-t.forward(100)
-t.left(135)
-t.forward(141.4)
-t.left(135)
-t.forward(100)
-turtle.done()
--- 2.2.bmi_nwj2.py
+++
@@ -1,4 +0,0 @@
-height = int(input("키를 입력하세요(cm) : "))
-weight = float(input("몸무게를 입력하세요(kg) : "))
-bmi = weight / (height/100) **2
-print("당신의 BMI는", round(bmi,2), "입니다.")
